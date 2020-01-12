ad48667c965dae9fafcaca52ad1a4603595f691b
xiaowei6078 <1218706950@qq.com>
2020-01-12 21:11:33 +0800

    修改conf

--- vue.config.js
+++ vue.config.js
@@ -25,10 +25,10 @@ module.exports = {
             "/api": {
                 //如果你的前端应用和后端 API 服务器没有运行在同一个主机上，你需要在开发环境下将 API 请求代理到 API 服务器。
                 target: "http://localhost:9091",
-                changeOrigin: true // 允许websockets跨域
-                // pathRewrite: {
-                //     "^/api": ""
-                // }
+                changeOrigin: true, // 允许websockets跨域
+                pathRewrite: {
+                    "^/api": ""
+                }
             }
         }
     },
